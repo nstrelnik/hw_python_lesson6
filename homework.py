5dbac29690b7609250cd9a1302b5f83f18e8fe02
Anastasiia Strelnik <n.strelnik@mail.ru>
2024-07-16 13:57:50 +0200

    Исправлено время в условиях

--- homework.py
+++ homework.py
@@ -6,10 +6,9 @@ def test_dark_theme_by_time():
     Протестируйте правильность переключения темной темы на сайте в зависимости от времени
     """
     current_time = time(hour=23)
-    is_dark_theme = None
     # TODO переключите темную тему в зависимости от времени суток (с 22 до 6 часов утра - ночь)
 
-    if time(hour=6) <= current_time <= time(hour=21):
+    if time(hour=6) <= current_time < time(hour=22):
         is_dark_theme = False
     else:
         is_dark_theme = True
@@ -37,7 +36,7 @@ def test_dark_theme_by_time_and_user_choice():
     elif dark_theme_enabled_by_user is False:
         is_dark_theme = False
     elif dark_theme_enabled_by_user is None:
-        if time(hour=6) <= current_time <= time(hour=21):
+        if time(hour=6) <= current_time < time(hour=22):
             is_dark_theme = False
         else:
             is_dark_theme = True
